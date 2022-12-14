ce34a6e2e6008cd0d0b4b98bc2dba15bf2e11049
LUCAS SOARES DO REGO <lucas.raziel@gmail.com>
2022-12-13 23:51:03 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': ['@swc/jest'],
+  },
+  clearMocks: true,
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
